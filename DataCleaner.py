a72a449ab91b84a577803282d17de88042ce59ed
keikeichor <66743465+keikeichor@users.noreply.github.com>
2021-07-03 20:32:11 -0400

    cleared a bug before int_df

--- DataCleaner.py
+++ DataCleaner.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[520]:
+# In[1]:
 
 
 #import mysql.connector as sq
 
 
-# In[519]:
+# In[2]:
 
 
 #mydb = sq.connect(host = "localhost",user = "root", password = "123456789!", buffered = True,
 #                  auth_plugin='mysql_native_password', allow_local_infile=True)
 
 
-# In[64]:
+# In[3]:
 
 
 #mycursor = mydb.cursor()
 
 
-# In[66]:
+# In[4]:
 
 
 #mycursor.execute("CREATE DATABASE NJ;")
 
 
-# In[67]:
+# In[5]:
 
 
 # mycursor.execute("CREATE TABLE NJ.teacher (last_name VARCHAR(50), \
@@ -37,7 +37,7 @@
 # experience_district INT,experience_nj INT,experience_total INT);")
 
 
-# In[68]:
+# In[6]:
 
 
 # mycursor.execute("LOAD DATA LOCAL INFILE '/Users/kkc/Desktop/harvard/Python for Engineering/week2/nj_state_teachers_salaries.csv' \
@@ -47,32 +47,32 @@
 #                  IGNORE 1 ROWS;")
 
 
-# In[69]:
+# In[7]:
 
 
 #mycursor.execute("SHOW TABLES FROM NJ;")
 
 
-# In[521]:
+# In[8]:
 
 
 # for db in mycursor:
 #     print(db)
 
 
-# In[522]:
+# In[9]:
 
 
 #mycursor.execute("USE NJ;")
 
 
-# In[523]:
+# In[10]:
 
 
 #mycursor.execute("SELECT * FROM teacher;")
 
 
-# In[524]:
+# In[11]:
 
 
 # for db in mycursor:
@@ -87,21 +87,21 @@
 
 # ### Load csv data into Pandas DataFrame:
 
-# In[85]:
+# In[12]:
 
 
 import pandas as pd
 import numpy as np
 
 
-# In[526]:
+# In[13]:
 
 
 teacher_df = pd.read_csv(r'nj_state_teachers_salaries.csv', low_memory=False) 
 teacher_df.head(5)
 
 
-# In[499]:
+# In[14]:
 
 
 teacher_df.columns
@@ -109,7 +109,7 @@ teacher_df.columns
 
 # ###  <font color='green'> Comment: the column names are correct. </font>
 
-# In[500]:
+# In[15]:
 
 
 teacher_df.dtypes
@@ -117,7 +117,7 @@ teacher_df.dtypes
 
 # ###  <font color='green'> Comment: there are inconsistent data types in column #6, #12, #13, #14, and most values are identified as 'object' rather than string, integers and floats. </font>
 
-# In[501]:
+# In[16]:
 
 
 teacher_df.isna().sum()
@@ -127,7 +127,7 @@ teacher_df.isna().sum()
 
 # ###  <font color='green'> if we randomly select one variable, 'district' == NaN, we can see there are at least 9 entries: </font>
 
-# In[502]:
+# In[17]:
 
 
 teacher_df[teacher_df['district'].isna()]
@@ -141,20 +141,20 @@ teacher_df[teacher_df['district'].isna()]
 
 # ###  <font color='green'> cleaning the data step1 - dropping the rows with at least one NaN values: </font>
 
-# In[503]:
+# In[18]:
 
 
 teacher_df_cleaned = teacher_df.dropna() 
 teacher_df_cleaned.head(5)
 
 
-# In[504]:
+# In[19]:
 
 
 print( 'dropped', teacher_df.count().max() - teacher_df_cleaned.count()[1], 'rows with NaN values')
 
 
-# In[505]:
+# In[20]:
 
 
 print('total rows without NAN values are:', teacher_df_cleaned.count()[0])
@@ -168,7 +168,7 @@ print('total rows without NAN values are:', teacher_df_cleaned.count()[0])
 
 # ###  <font color='green'> cleaning the data step2 - convert the string values with leading and tailing spaces trimmed: </font>
 
-# In[506]:
+# In[21]:
 
 
 pd.options.mode.chained_assignment = None 
@@ -181,19 +181,19 @@ for i in str_list:
     teacher_df_cleaned[i] = teacher_df_cleaned[i].str.strip()
 
 
-# In[507]:
+# In[22]:
 
 
 teacher_df_cleaned.head(5)
 
 
-# In[508]:
+# In[23]:
 
 
 teacher_df_cleaned.loc[teacher_df_cleaned['last_name'].str.strip().str[0]== ' ']
 
 
-# In[509]:
+# In[24]:
 
 
 teacher_df_cleaned.loc[teacher_df_cleaned['county'].str.strip().str[-1]== ' ']
@@ -217,7 +217,7 @@ teacher_df_cleaned.loc[teacher_df_cleaned['county'].str.strip().str[-1]== ' ']
 
 # ###  <font color='green'> step 3.1 - we first need to check if there's any strange, non-numeric values. We convert the numeric objects to float values as to be safe, some of the numeric variables are integers, but floats will include those. However, when converting the objects to floats, the errors should be 'coerce' then the non-numeric values will be replaced with NaN: </font>
 
-# In[510]:
+# In[25]:
 
 
 num_list = ['fte', 'experience_district', 'experience_nj', 'experience_total' ]
@@ -228,31 +228,31 @@ for n in num_list:
 
 # ###  <font color='green'> the new NaN values are those that contain non-numeric values, as shown below, there are 10 non-numeric values identified and marked as NaN. and we drop the NAs again to have the final clean data: </font>
 
-# In[511]:
+# In[26]:
 
 
 teacher_df_cleaned.isna().sum()
 
 
-# In[512]:
+# In[27]:
 
 
 teacher_df_cleaned = teacher_df_cleaned.dropna()
 
 
-# In[514]:
+# In[28]:
 
 
 teacher_df_cleaned.isna().sum()
 
 
-# In[515]:
+# In[29]:
 
 
 teacher_df_cleaned.count()
 
 
-# In[513]:
+# In[30]:
 
 
 print('final rows without NaNs and non-numeric values are:', teacher_df_cleaned.count()[0])
@@ -264,7 +264,7 @@ print('final rows without NaNs and non-numeric values are:', teacher_df_cleaned.
 
 
 
-# In[516]:
+# In[31]:
 
 
 teacher_df_cleaned.dtypes
@@ -278,7 +278,19 @@ teacher_df_cleaned.dtypes
 
 # ###  <font color='green'> step 3.2 - if we want to be more specific whether the numeric values are integers or floats, we can first convert the float values into string, and strip out the last value to see if it is zero: </font>
 
-# In[486]:
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[32]:
 
 
 int_pd = pd.DataFrame()
@@ -286,12 +298,12 @@ int_pd = pd.DataFrame()
 int_check_list = ['fte','experience_district', 'experience_nj', 'experience_total']
 
 for m in int_check_list:
-    int_pd[m] = pd.DataFrame(teacher_df_cleaned[m].astype(pd.StringDtype()).str.strip().str[-1]== '0' )
+    int_pd[m] = teacher_df_cleaned[m].astype(pd.StringDtype()).str.strip().str[-1]== '0' 
 
 
 # ###  <font color='green'> combine the stripped-out values into a dataframe, 'True' means the last value is 0, so this is an integer, otherwise it is a float: </font>
 
-# In[487]:
+# In[33]:
 
 
 int_pd.tail(5)
@@ -299,32 +311,32 @@ int_pd.tail(5)
 
 # ###  <font color='green'> apparently there are non-zero values in this dataframe: </font>
 
-# In[488]:
+# In[34]:
 
 
 int_pd[(int_pd['experience_total']== False) | (int_pd['fte']== False) |  (int_pd['experience_district']== False)
       | (int_pd['experience_nj']== False) ]
 
 
-# In[489]:
+# In[35]:
 
 
 int_pd['fte'].unique()
 
 
-# In[490]:
+# In[36]:
 
 
 int_pd['experience_total'].unique()
 
 
-# In[491]:
+# In[37]:
 
 
 int_pd['experience_district'].unique()
 
 
-# In[492]:
+# In[38]:
 
 
 int_pd['experience_nj'].unique()
@@ -338,7 +350,7 @@ int_pd['experience_nj'].unique()
 
 
 
-# In[493]:
+# In[39]:
 
 
 teacher_df_cleaned.head(5)
@@ -350,7 +362,7 @@ teacher_df_cleaned.head(5)
 
 
 
-# In[495]:
+# In[40]:
 
 
 teacher_df_cleaned.to_csv('new_nj_state_teachers_salaries.csv', index= False)
